f3c9449d3617f9c1a2ba74ee8d16696aa404a981
DharanishTR <dharanish9696@gmail.com>
2024-04-25 15:40:21 +0530

    First commit

---
+++ src/main/java/com/example/salesLaborDriver/SalesLaborDriverApplication.java
@@ -0,0 +1,13 @@
+package com.example.salesLaborDriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesLaborDriverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesLaborDriverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/salesLaborDriver/SalesLaborDriverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salesLaborDriver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesLaborDriverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
